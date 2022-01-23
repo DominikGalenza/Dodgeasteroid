6e5a6bb038752fb228cc4160bc0758305f63187e
Dominik Gałenza <dominik.galenza@gmail.com>
2022-01-23 20:33:52 +0100

    Added screen wrapping

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -21,6 +21,19 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        ProcessInput();
+        KeepPlayerOnScreen();
+    }
+
+    void FixedUpdate()
+    {
+        if(movementDirection == Vector3.zero) { return; }
+        rb.AddForce(movementDirection * forceMagnitude, ForceMode.Force);
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxVelocity);
+    }
+
+    private void ProcessInput()
+    {
         if(Touchscreen.current.primaryTouch.press.isPressed)
         {
             Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
@@ -35,10 +48,28 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    void FixedUpdate()
+    private void KeepPlayerOnScreen()
     {
-        if(movementDirection == Vector3.zero) { return; }
-        rb.AddForce(movementDirection * forceMagnitude, ForceMode.Force);
-        rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxVelocity);
+        Vector3 newPosition = transform.position;
+        Vector3 viewportPosition = mainCamera.WorldToViewportPoint(transform.position);
+
+        if(viewportPosition.x > 1)
+        {
+            newPosition.x = -newPosition.x + 0.1f;
+        }
+        else if(viewportPosition.x < 0)
+        {
+            newPosition.x = -newPosition.x - 0.1f;
+        }
+
+        if(viewportPosition.y > 1)
+        {
+            newPosition.y = -newPosition.y + 0.1f;
+        }
+        else if(viewportPosition.y < 0)
+        {
+            newPosition.y = -newPosition.y - 0.1f;
+        }
+        transform.position = newPosition;
     }
 }
